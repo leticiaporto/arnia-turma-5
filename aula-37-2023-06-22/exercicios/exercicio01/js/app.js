7afb16fe74695332699a2a467f6a6cc29994d43f
leticiaporto <leticiaportosoares@gmail.com>
2023-06-23 16:05:15 -0300

    feat: aula 37 solution

---
+++ aula-37-2023-06-22/exercicios/exercicio01/js/app.js
@@ -0,0 +1,43 @@
+/* Variável que armazena o objeto DOM que representa o modal */
+const modal = document.getElementById("modal")
+
+/* 
+    Função responsável por montar uma string com o HTML das notícias e depois adicioná-lo à página
+*/
+const renderizarNoticias = (noticias) => {
+    const content = document.getElementById('content')
+    let conteudo = ''
+
+    noticias.forEach((noticia) => {
+        conteudo = conteudo + `
+      <div class="card">
+        <div class="image">
+          <img src="${noticia.imagem}" />
+        </div>
+        <div class="card-content">
+          <div class="card-texts">
+            <div class="card-title">
+              ${noticia.titulo}
+            </div>
+            <div class="card-text">
+              ${noticia.texto}
+            </div>
+          </div>
+        </div>
+      </div>
+    `
+    })
+
+    content.innerHTML = conteudo
+}
+
+/* 
+    Função responsável por buscar as notícias na API
+*/
+const getNoticias = async () => {
+    const apiResponse = await fetch('http://localhost:3000/noticias')
+    const noticias = await apiResponse.json()
+    renderizarNoticias(noticias)
+}
+
+getNoticias()
